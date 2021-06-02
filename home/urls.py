c617d1735daac80047cf69dc21d0837d1edebb34
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-06-02 12:46:18 -0300

    added view and url new_movie

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:movie_id>', views.detail, name='detail'),
+    path("", views.index, name="index"),
+    path("<int:movie_id>", views.detail, name="detail"),
+    path("new_movie", views.new_movie, name="new_movie"),
 ]
--- home/views.py
+++ home/views.py
@@ -10,3 +10,7 @@ def index(request):
 def detail(request, movie_id):
     movie = get_object_or_404(Movies, id=movie_id)
     return render(request, "home/detail.html", {"movie": movie})
+
+
+def new_movie(request):
+    return render(request, "home/new_movie.html")
