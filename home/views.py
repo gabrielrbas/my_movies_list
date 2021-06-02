cc1c080cbce930dee1380bfbd0fc59e32d1017c3
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-06-01 21:52:59 -0300

    added movies objects in views.py

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Movies
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    movies = Movies.objects.all()
+    return render(request, 'home/index.html', {
+        'movies': movies
+    })
