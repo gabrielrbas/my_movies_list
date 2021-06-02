920e2ec19489d07977878883f8399642de233205
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-06-02 11:00:35 -0300

    added func detail in home views.py

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movies
 
 
 def index(request):
     movies = Movies.objects.all()
-    return render(request, 'home/index.html', {
-        'movies': movies
-    })
+    return render(request, "home/index.html", {"movies": movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movies, id=movie_id)
+    return render(request, "home/detail.html", {"movie": movie})
