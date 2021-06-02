f1c7334802c1ae57d0e8370c71f7c9c851ad4518
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-06-01 21:30:35 -0300

    updated models

--- home/models.py
+++ home/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Movies(models.Model):
-    pic = models.ImageField(blank=True, null=True)
+    pic = models.ImageField(
+        upload_to='movies_img/%Y/%m', blank=True, null=True)
     name = models.CharField(max_length=255)
     whatched = models.BooleanField(default=False)
     date = models.DateField(blank=True, null=True)
